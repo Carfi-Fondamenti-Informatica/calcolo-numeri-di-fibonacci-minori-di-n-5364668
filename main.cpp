f3c85477b3b20bf3990106fbce9c05205f9a0ca9
5364668 <92469272+5364668@users.noreply.github.com>
2021-11-13 20:43:27 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,18 @@
 using namespace std;
 
 int main() {
+   int n=0,a=0,b=1,c=a+b;
+   cin>>n;
+   
+   cout<<b<<endl;
+   for (int i=0;i<n;i++) {
+      if (c<=n){
+         cout<<c<<endl;
+         a=b;
+         b=c;
+         c=a+b;
+      }
+      
+      }     
    return 0;
 }
